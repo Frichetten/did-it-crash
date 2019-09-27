8bcf508f1bb78799cdd0a26d4e622c4a35b7e8c2
Nick Frichette <nick@frichetten.com>
2019-09-27 13:46:29 -0500

    Added multi site support. Set the url to include a ; as a delimeter

--- index.js
+++ index.js
@@ -3,26 +3,28 @@ const request = require('request');
 const url = process.env['url'];
 
 exports.handler = function(event,context) {
-  request.get(url, (err, response, body) => {
+  url.split(";").forEach(function(address) {
+    request.get(address, (err, response, body) => {
     
-    if (response && response.statusCode == 200 && !err) {
-      console.log(url+ ' is up');
-    }
-    else {
-      var sns = new aws.SNS();
-      console.log(url + ' is down!');
-      sns.publish({
-        Message: url + ' is down! You should go fix it when you can!',
-        TopicArn: process.env['DidItCrashSNSArn']
-        }, function(err, data) {
-          if (err) {
-            console.log(err.stack);
-	    context.done(err, 'This function ended in an error!');
-	    return;
-          }
+      if (response && response.statusCode == 200 && !err) {
+        console.log(address+ ' is up');
+      }
+      else {
+        var sns = new aws.SNS();
+        console.log(address + ' is down!');
+        sns.publish({
+          Message: address + ' is down! You should go fix it when you can!',
+          TopicArn: process.env['DidItCrashSNSArn']
+          }, function(err, data) {
+            if (err) {
+              console.log(err.stack);
+	      context.done(err, 'This function ended in an error!');
+	      return;
+            }
           console.log(data);
           context.done(null, 'Function successfully terminated!');
-      });
-    }
+        });
+      }
+    });
   });
 };
